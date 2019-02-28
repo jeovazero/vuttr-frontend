ea98217638b462e103cb041f8519e92658e940ec
jeovazero <jeovapereira2000@live.com>
2019-02-28 14:58:49 -0300

    feat(modal): add component and stories

---
+++ src/components/atoms/Modal/Modal.jsx
@@ -0,0 +1,34 @@
+/** @flow */
+/** @jsx jsx */
+import styled from '@emotion/styled'
+import { css, jsx } from '@emotion/core'
+
+type Props = {
+  /** Flag isOpen */
+  isOpen: boolean
+}
+
+const style = ({ theme, isOpen }) => css`
+  display: ${isOpen ? 'block' : 'none'};
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+  left: 0;
+  margin: 0;
+  border: 0;
+  background-color: rgba(0, 0, 0, 0.25);
+`
+
+const StyledModal = styled.div`
+  ${style}
+`
+
+/** Modal component */
+const Modal = (props: Props) => <StyledModal {...props} />
+
+Modal.defaultProps = {
+  isOpen: false
+}
+
+export default Modal
