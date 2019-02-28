3fb1a3021bf3934a62d0b4cf1a710a5251d4203e
jeovazero <jeovapereira2000@live.com>
2019-02-28 13:00:24 -0300

    feat(input): add component and stories

---
+++ src/components/atoms/Input/Input.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Input from './Input'
+import { storiesOf } from '@storybook/react'
+import { select, text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+
+const group = 'GROUP-INPUT'
+const iconOptions = [null, 'plus', 'checkmark', 'close', 'search']
+const sizeOptions = ['small', 'medium', 'large']
+const colorOptions = ['Ink', 'DarkBlue', 'Blue', 'Teal']
+
+storiesOf('Atoms', module).add('Input', () => {
+  const icon = select('prefixIcon', iconOptions, null, group)
+  const size = select('size', sizeOptions, 'medium', group)
+  const color = select('color', colorOptions, 'Blue', group)
+  const value = text('Value', 'value', group)
+  const placeholder = text('Placeholder', 'Digite...', group)
+  return (
+    <Input
+      prefixIcon={icon}
+      size={size}
+      color={color}
+      value={value}
+      onChange={action('[Input]: onChange!')}
+      onBlur={action('[Input]: onBlur!')}
+      onFocus={action('[Input]: onFocus!')}
+      placeholder={placeholder}
+    />
+  )
+})
