98396e407f1cacd43c3a07469a69bf2479298aac
jeovazero <jeovapereira2000@live.com>
2019-02-28 12:16:39 -0300

    feat(tag): add component and stories

---
+++ src/components/atoms/Tag/Tag.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Tag from './Tag'
+import { storiesOf } from '@storybook/react'
+import { select, text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+
+const group = 'GROUP-TAG'
+const sizeOptions = ['small', 'medium', 'large']
+const colorOptions = ['Ink', 'DarkBlue', 'Blue', 'LightBlue', 'Teal', 'White']
+
+storiesOf('Atoms', module).add('Tag', () => {
+  const size = select('size', sizeOptions, 'medium', group)
+  const color = select('color', colorOptions, 'Blue', group)
+  const label = text('Label', 'Tag', group)
+
+  return (
+    <Tag
+      color={color}
+      size={size}
+      label={label}
+      onClick={action('[Tag]: clicked!')}
+    />
+  )
+})
